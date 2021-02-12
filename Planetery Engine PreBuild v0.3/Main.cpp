25285169c14a9df0ba3b18c8512ab54d3af23438
tom lee <tomlee92502@yahoo.com>
2021-02-12 11:49:09 +0800

    UTF-8 Support: Added utf-8 test string. Now starting to add utf-8 decoding to font engine...

--- Planetery Engine PreBuild v0.3/Main.cpp
+++ Planetery Engine PreBuild v0.3/Main.cpp
@@ -1,4 +1,4 @@
-#include "Logger.h"
+﻿#include "Logger.h"
 #include "ThreadEvents.h"
 #include "ThreadRender.h"
 #include "Font.h"
@@ -9,6 +9,44 @@
 #include <iostream>
 #include <chrono>
 #include <atomic>
+
+constexpr const char* UTF_8_TEST_STRING = R"V0G0N(English: The quick brown fox jumps over the lazy dog.
+Jamaican: Chruu, a kwik di kwik brong fox a jomp huova di liezi daag de, yu no siit ?
+Irish : "An ḃfuil do ċroí ag bualaḋ ó ḟaitíos an ġrá a ṁeall lena ṗóg éada ó ṡlí do leasa ṫú?" "D'ḟuascail Íosa Úrṁac na hÓiġe Beannaiṫe pór Éava agus Áḋaiṁ."
+Dutch : Pa's wĳze lynx bezag vroom het fikse aquaduct.
+German : Falsches Üben von Xylophonmusik quält jeden größeren Zwerg. (1)
+German : Im finſteren Jagdſchloß am offenen Felsquellwaſſer patzte der affig-flatterhafte kauzig-höf‌liche Bäcker über ſeinem verſifften kniffligen C-Xylophon. (2)
+Norwegian : Blåbærsyltetøy("blueberry jam", includes every extra letter used in Norwegian).
+Swedish : Flygande bäckasiner söka strax hwila på mjuka tuvor.
+Icelandic : Sævör grét áðan því úlpan var ónýt.
+Finnish : (5) Törkylempijävongahdus(This is a perfect pangram, every letter appears only once.Translating it is an art on its own, but I'll say "rude lover's yelp". :-D)
+	Finnish: (5) Albert osti fagotin ja töräytti puhkuvan melodian. (Albert bought a bassoon and hooted an impressive melody.)
+	Finnish : (5) On sangen hauskaa, että polkupyörä on maanteiden jokapäiväinen ilmiö. (It's pleasantly amusing, that the bicycle is an everyday sight on the roads.)
+		Polish: Pchnąć w tę łódź jeża lub osiem skrzyń fig.
+		Czech : Příliš žluťoučký kůň úpěl ďábelské ódy.
+		Slovak : Starý kôň na hŕbe kníh žuje tíško povädnuté ruže, na stĺpe sa ďateľ učí kvákať novú ódu o živote.
+		Slovenian : Šerif bo za domačo vajo spet kuhal žgance.
+		Greek(monotonic) : ξεσκεπάζω την ψυχοφθόρα βδελυγμία
+		Greek(polytonic) : ξεσκεπάζω τὴν ψυχοφθόρα βδελυγμία
+		Russian : Съешь же ещё этих мягких французских булок да выпей чаю.
+		Russian : В чащах юга жил-был цитрус ? Да, но фальшивый экземпляр!ёъ.
+		Bulgarian : Жълтата дюля беше щастлива, че пухът, който цъфна, замръзна като гьон.
+		Sami(Northern) : Vuol Ruoŧa geđggiid leat máŋga luosa ja čuovžža.
+		Hungarian : Árvíztűrő tükörfúrógép.
+		Spanish : El pingüino Wenceslao hizo kilómetros bajo exhaustiva lluvia y frío, añoraba a su querido cachorro.
+		Spanish : Volé cigüeña que jamás cruzó París, exhibe flor de kiwi y atún.
+		Portuguese : O próximo vôo à noite sobre o Atlântico, põe freqüentemente o único médico. (3)
+		French : Les naïfs ægithales hâtifs pondant à Noël où il gèle sont sûrs d'être déçus en voyant leurs drôles d'œufs abîmés.
+		Esperanto : Eĥoŝanĝo ĉiuĵaŭde
+		Esperanto : Laŭ Ludoviko Zamenhof bongustas freŝa ĉeĥa manĝaĵo kun spicoj.
+		Hebrew : זה כיף סתם לשמוע איך תנצח קרפד עץ טוב בגן.
+		Japanese(Hiragana) :
+		いろはにほへど　ちりぬるを
+		わがよたれぞ　つねならむ
+		うゐのおくやま　けふこえて
+		あさきゆめみじ　ゑひもせず(4))V0G0N";
+
+
 
 static StringBox* listTest = nullptr;
 
@@ -78,9 +116,7 @@ int main() {
 			listTest->setSize(vec2(1, 1));
 			listTest->setTextSize(12.f);
 			listTest->clear();
-			for (uint i = 0; i<128; i++) {
-				*listTest << i << ": Hello~ How are you doing?\n";
-			}
+			*listTest << UTF_8_TEST_STRING;
 		}
 		if (events::ThreadEvents::isWindowResized() || events::ThreadEvents::isWindowMoved()) {
 			listTest->notifyPPIChanged();
